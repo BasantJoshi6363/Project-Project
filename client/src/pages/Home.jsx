f2f002d42ae35a470da36789374c32ecb77d8903
BasantJoshi6363 <basantjoshi6363@gmail.com>
2024-11-09 10:24:10 +0545

    added a shadcn ui

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,8 @@
 import React from 'react'
-import UpperNavbar from '../components/UpperNavbar'
-import Navbar from '../components/Navbar'
-import Categories from '../components/Categories'
-import Slider from '../components/Slider'
 
 const Home = () => {
   return (
-    <div className=' font-[#fefefe]'>
-        <UpperNavbar />
-        <Navbar />
-        <div className="carouselNCategories flex">
-          <Categories />  
-          <Slider />
-        </div>
-    </div>
+    <div>Home</div>
   )
 }
 
